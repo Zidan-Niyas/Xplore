af770c20ed8564a7378e571e15fc118474f6efc8
Zidan <muhdzidan007@gmail.com>
2025-01-31 14:00:01 +0530

    some change

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -31,7 +31,7 @@ const Events = () => {
         setFilteredEvents(eventData.filter((event) => event.category === "event"))
         break
       default:
-        setFilteredEvents(eventData)
+        setFilteredEvents(competitionData)
     }
   }, [eventType])
 
